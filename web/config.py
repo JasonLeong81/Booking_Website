5938c9d82ad6c3f8ea89ebdd37225562c5d95146
Jason <leongjason822@gmail.com>
2021-03-03 11:43:40 +0800

    m

--- web/config.py
+++ web/config.py
@@ -1,9 +1,9 @@
 import os
 class Config:
     SECRET_KEY = 'jason'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///jason.db'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///jason.db'
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    # SQLALCHEMY_DATABASE_URI ='postgres://ffssqvauahbwit:abfcd75994bb9fcebb8cac1eb5c9b7078d82929c6a5245b0500e6c22b6d06223@ec2-34-254-69-72.eu-west-1.compute.amazonaws.com:5432/d8959l3lb2bqep'
+    SQLALCHEMY_DATABASE_URI ='postgres://ysrqfbbqcroxrk:7836ec49050c77b20c5ef5a7ae20afd3ca6995113083ee5a8de647af65218915@ec2-54-74-14-109.eu-west-1.compute.amazonaws.com:5432/dfbp68fivhnfte'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
